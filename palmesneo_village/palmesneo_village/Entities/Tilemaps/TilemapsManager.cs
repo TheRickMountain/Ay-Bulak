025c15e6225732cab91d5c930a4c64f5ec3372ed
TheRickMountain <mountnrickrose@gmail.com>
2025-03-02 13:00:17 +0100

    Temporaly Building system removal

--- palmesneo_village/palmesneo_village/Entities/Tilemaps/TilemapsManager.cs
+++ palmesneo_village/palmesneo_village/Entities/Tilemaps/TilemapsManager.cs
@@ -7,37 +7,9 @@ namespace palmesneo_village
     {
         private Tilemap groundTilemap;
 
-        private Building[,] buildings;
-
         public TilemapsManager(Tilemap groundTilemap)
         {
             this.groundTilemap = groundTilemap;
-
-            buildings = new Building[groundTilemap.TileColumns, groundTilemap.TileRows];
         }
-
-        #region Building
-
-        public void SetBuilding(Vector2 tile, Building building)
-        {
-            if (buildings[(int)tile.X, (int)tile.Y] != null)
-            {
-                throw new Exception("The tile is already occupied!");
-            }
-
-            buildings[(int)tile.X, (int)tile.Y] = building;
-        }
-
-        public void ClearBuilding(Vector2 tile)
-        {
-            buildings[(int)tile.X, (int)tile.Y] = null;
-        }
-
-        public Building GetBuilding(Vector2 tile)
-        {
-            return buildings[(int)tile.X, (int)tile.Y];
-        }
-
-        #endregion
     }
 }
