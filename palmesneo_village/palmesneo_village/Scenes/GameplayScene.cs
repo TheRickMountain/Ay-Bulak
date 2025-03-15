fd6ced35b3bffe339245f98c998c0eb85c2f9115
TheRickMountain <mountnrickrose@gmail.com>
2025-03-16 00:06:54 +0100

    Добавление свеклы

--- palmesneo_village/palmesneo_village/Scenes/GameplayScene.cs
+++ palmesneo_village/palmesneo_village/Scenes/GameplayScene.cs
@@ -274,7 +274,8 @@ namespace palmesneo_village
                                 Engine.ItemsDatabase.GetItemByName<SeedItem>("garlic_seeds"),
                                 Engine.ItemsDatabase.GetItemByName<SeedItem>("eggplant_seeds"),
                                 Engine.ItemsDatabase.GetItemByName<SeedItem>("carrot_seeds"),
-                                Engine.ItemsDatabase.GetItemByName<SeedItem>("cabbage_seeds")
+                                Engine.ItemsDatabase.GetItemByName<SeedItem>("cabbage_seeds"),
+                                Engine.ItemsDatabase.GetItemByName<SeedItem>("beetroot_seeds")
                             });
 
                             gameState = GameState.Trading;
