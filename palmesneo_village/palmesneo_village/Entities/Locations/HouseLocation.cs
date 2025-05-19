d7693c6c51bbd7b3dc0c8703180acec5d1aac9f4
TheRickMountain <mountnrickrose@gmail.com>
2025-05-19 14:19:04 +0200

    Изменение размера локации дома игрока

--- palmesneo_village/palmesneo_village/Entities/Locations/HouseLocation.cs
+++ palmesneo_village/palmesneo_village/Entities/Locations/HouseLocation.cs
@@ -9,52 +9,34 @@ namespace palmesneo_village
     {
 
         public HouseLocation(string id, Teleport exitTeleport, TimeOfDayManager timeOfDayManager) 
-            : base(id, 32, 16, false, timeOfDayManager)
+            : base(id, 16, 16, false, timeOfDayManager)
         {
             CreateHouseFirstLayer();
 
-            BuildingItem singleBed = Engine.ItemsDatabase.GetItemByName<BuildingItem>("single_bed");
-            TryBuild(singleBed, 6, 5, Direction.Down);
-
-            BuildingItem stove = Engine.ItemsDatabase.GetItemByName<BuildingItem>("stove");
-            TryBuild(stove, 20, 5, Direction.Down);
-
-            BuildingItem washBasin = Engine.ItemsDatabase.GetItemByName<BuildingItem>("wash_basin");
-            TryBuild(washBasin, 23, 5, Direction.Down);
-
-            BuildingItem gasStove = Engine.ItemsDatabase.GetItemByName<BuildingItem>("gas_stove");
-            TryBuild(gasStove, 24, 5, Direction.Down);
-
-            BuildingItem fridge = Engine.ItemsDatabase.GetItemByName<BuildingItem>("fridge");
-            TryBuild(fridge, 25, 5, Direction.Down);
-
+            TryBuild(Engine.ItemsDatabase.GetItemByName<BuildingItem>("window"), 6, 2, Direction.Down);
             TryBuild(Engine.ItemsDatabase.GetItemByName<BuildingItem>("window"), 9, 2, Direction.Down);
-            TryBuild(Engine.ItemsDatabase.GetItemByName<BuildingItem>("window"), 14, 2, Direction.Down);
-            TryBuild(Engine.ItemsDatabase.GetItemByName<BuildingItem>("window"), 23, 2, Direction.Down);
 
-            TryBuild(Engine.ItemsDatabase.GetItemByName<BuildingItem>("table"), 24, 7, Direction.Down);
-
-            CreateTeleport(23, 10, exitTeleport);
+            CreateTeleport(9, 13, exitTeleport);
         }
 
         private void CreateHouseFirstLayer()
         {
-            string rawMap = "********************************\n" +
-                         "********************************\n" +
-                         "******555555555555**555555******\n" +
-                         "******555555555555**555555******\n" +
-                         "******55555555555555555555******\n" +
-                         "******44444444444455444444******\n" +
-                         "******44444444444455444444******\n" +
-                         "******44444444444444444444******\n" +
-                         "******44444444444444444444******\n" +
-                         "******444444444444**444444******\n" +
-                         "***********************4********\n" +
-                         "********************************\n" +
-                         "********************************\n" +
-                         "********************************\n" +
-                         "********************************\n" +
-                         "********************************\n";
+            string rawMap = "****************\n" +
+                            "****************\n" +
+                            "****55555555****\n" +
+                            "****55555555****\n" +
+                            "****55555555****\n" +
+                            "****44444444****\n" +
+                            "****44444444****\n" +
+                            "****44444444****\n" +
+                            "****44444444****\n" +
+                            "****44444444****\n" +
+                            "****44444444****\n" +
+                            "****44444444****\n" +
+                            "****44444444****\n" +
+                            "*********4******\n" +
+                            "****************\n" +
+                            "****************\n";
 
             char[,] map = new char[MapWidth, MapHeight];
 
