e3ac5caa5c93ebde884a95f3213c3cccb0caaa80
TheRickMountain <mountnrickrose@gmail.com>
2025-10-26 15:17:33 +0100

    Displaying debug lines in DevScene

--- palmesneo_village/palmesneo_village/Scenes/DevScene.cs
+++ palmesneo_village/palmesneo_village/Scenes/DevScene.cs
@@ -9,13 +9,22 @@ namespace palmesneo_village
 
         public override void Begin()
         {
-
             base.Begin();
         }
 
         public override void Update()
         {
             base.Update();
+        }
+
+        public override void Render()
+        {
+            base.Render();
+
+            // Debug lines
+            float lineThickness = 1.0f / Engine.Camera.Zoom.X;
+            RenderManager.Line(new Vector2(0, -10000), new Vector2(0, 10000), Color.Red * 0.5f, lineThickness);
+            RenderManager.Line(new Vector2(-10000, 0), new Vector2(10000, 0), Color.Green * 0.5f, lineThickness);
         }
 
 
